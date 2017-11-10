f18ecb70ff3d4985a48aa150ff413991f8418f6b
Draven072 <mdmitry072@gmail.com>
2017-11-10 23:44:58 +0100

    111

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 ansible-playbook 1-digiocean_host_create.yml
 cat newhost >> hosts
-#ssh-keyscan  $(cat newhost) >> ~/.ssh/known_hosts
-#ansible-playbook 2-digiocean_docker_install.yml
+sleep 20
 
-#ADDR=$(cat newhost)
-#sed -i -e "s/$ADDR//" hosts
-#echo -n > newhost
+ansible-playbook 2-digiocean_docker-ce_install.yml
+sleep 10
+
+ansible-playbook 3-digiocean-dockers.yml
+
+ADDR=$(cat newhost)
+sed -i -e 's/$ADDR//' hosts
+sed -i '/./,/^$/!d' hosts
+echo -n > newhost
